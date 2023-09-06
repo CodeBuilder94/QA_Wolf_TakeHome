93076dbb5986b5ab547bed2116b56444bd975dff
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-09-06 12:51:01 -0500

    Got playwright installed trying to get the tests to run.

--- tests/netflix.spec.js
+++ tests/netflix.spec.js
@@ -8,7 +8,7 @@ test("my first test", async ({ page }) => {
   await page.goto("https://www.netflix.com");
 
   // assert page title appears
-  await expect(page.locator('[data-uia="hero-title"]')).toHaveText(
+  await expect(page.locator('[data-uia="nmhp-card-hero-title"]')).toHaveText(
     "Unlimited movies, TV shows, and more."
   );
 });
