9c968aa166abd5e51eaa3838bfcf6ae18b5f6fcd
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2023-09-08 13:00:13 -0500

    Figured out how to change headless. Go to sign in page and put in fake email.

--- tests/netflix.spec.js
+++ tests/netflix.spec.js
@@ -1,16 +1,31 @@
 const { test, expect } = require("@playwright/test");
 
 // run tests in headful mode so you can see the browser
-test.use({ headless: false, slowMo: 1000 });
+test.use({ headless: true, slowMo: 1000 });
 
 test("my first test", async ({ page }) => {
   // go to Netflix.com
   await page.goto("https://www.netflix.com");
 
   // assert page title appears
-  await expect(page.locator('[data-uia="nmhp-card-hero-title"]')).toHaveText(
+  await expect(page.locator('[data-uia="hero-title"]')).toHaveText(
     "Unlimited movies, TV shows, and more."
   );
 });
 
 // ADD YOUR TESTS HERE!
+test("bad sign in", async({page}) =>{
+  await page.goto("https://www.netflix.com");
+
+  //click sign in button to move to sing up page
+  await page.getByRole('button', {name: 'Sign In'}).click();
+  //enter fake email
+  //await expect(page.getByRole('input', {name: ""}))
+  await page.fill(".nfTextField", "testemail@test.com");
+  //enter fake password
+
+  //submit information
+
+  //test too see if the bad loging prompt appeared
+});
+
